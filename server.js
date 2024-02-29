faf84fa692ee68fe290e723bfd4f6aac6a12dcb8
san06032002 <sanskarpardesi@gmail.com>
2024-02-29 00:35:06 -0500

    files added

---
+++ server.js
@@ -0,0 +1,53 @@
+const legoData = require("./modules/legoSets");
+const path = require("path");
+
+const express = require('express');
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.get("/lego/sets", async (req,res)=>{
+
+  try{
+    if(req.query.theme){
+      let sets = await legoData.getSetsByTheme(req.query.theme);
+      res.send(sets);
+  
+    }else{
+      let sets = await legoData.getAllSets();
+      res.send(sets);
+    }
+  }catch(err){
+    res.status(404).send(err);
+  }
+
+});
+
+app.get("/lego/sets/:num", async (req,res)=>{
+  try{
+    let set = await legoData.getSetByNum(req.params.num);
+    res.send(set);
+  }catch(err){
+    res.status(404).send(err);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+});
+
+
+legoData.initialize().then(()=>{
+  app.listen(HTTP_PORT, () => { console.log(`server listening on: http://localhost:${HTTP_PORT}`) });
+});
